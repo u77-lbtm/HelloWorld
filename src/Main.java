90153fbb64ea290b0a4983cf36b7247ae4483652
u77-lbtm <kraftrr77@gmail.com>
2026-04-09 12:43:54 +0500

    Revert "Added output whit two languages"

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Привет Мир");
-        System.out.println("Hello World!");
+        System.out.printf("Hello World!");
 
 
     }
